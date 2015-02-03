9ff634637feac69c9c67b098a6ddede4653a3259
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2015-02-03 09:41:57 -0500

    Bons prédicats de version pour les paquetages.

--- setup.py
+++ setup.py
@@ -3,6 +3,4 @@ from distutils.core import setup
 setup(
     name='ift4055-',
     description='Reachability of petri nets and d-VASS',
-    requires=['numpy (1.9.*)','scipy (0.15.*)','nose']
-    
-)
+    requires=['numpy (>=1.9)','scipy (>=0.15)','nose'])
