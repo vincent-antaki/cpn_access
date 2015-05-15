8f4f953f477821c4f047d6829ff9bb9e6d98c73b
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2015-05-15 13:29:32 -0400

    Support exclusif pour Python 2
    Le module benchmarking n'est pas compatible sous Python 3 et le module
    python-qsoptex a été initialement écrit pour Python 2, alors c'est
    préférable de rester sur cette version du langage.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
@@ -7,5 +7,4 @@ setup(
     name='ift4055-projet-honor-Antaki',
     description='Algorithm for evaluation of the accessibilty problem in continuous petri nets',
     author='Vincent Antaki',
-    requires=['numpy (>=1.9)','scipy (>=0.15)','nose', 'python-qsoptex'],
-    dependency_link = ['https://github.com/DjAntaki/python-qsoptex'])
+    requires=['numpy (>=1.9)','scipy (>=0.15)','nose', 'python_qsoptex'])
